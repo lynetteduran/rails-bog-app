5f1f75b334f5c5277a5fa9f8c6f3a5425a7ac73a
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-10-18 09:35:44 -0700

    adds creature create method to creature controller

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -2,4 +2,14 @@ class CreaturesController < ApplicationController
     def index
       @creatures = Creature.all
     end
+    def create
+    	creature_params= params.require(:creature).permit(:name, :description)
+    	creature = Creature.new(creature_params)
+
+    	if creature.save
+    		redirect_to creatures_path
+    	end
+    end
+    def new
+    	@creature = Creature.new
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -5,5 +5,6 @@
     root "creatures#index"
 
     get "/creatures", to: "creatures#index", as: "creatures"
-
+	get "/creatures/new", to: "creatures#new", as: "new_creature"
+  	post "/creatures", to: "creatures#create"
   end
